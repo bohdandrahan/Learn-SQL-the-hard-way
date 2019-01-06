da49b1f89f1caed4d03bc8f2d677aaca4882f4dd
bohdandrahan <bohdan.drahan@gmail.com>
2019-01-06 14:29:24 -0500

    add ex6 study drills

--- ex6.sql
+++ ex6.sql
@@ -13,5 +13,29 @@ SELECT pet.id, pet.name, pet.age, pet.dead
 	(
 		SELECT pet_id FROM person_pet, person
 		WHERE person_pet.person_id = person.id
-		AND person.first_name = 'John'
+		AND person.first_name = 'John')
 	);
+/* Study drills
+	Python type class and objects relations
+
+CREATE TABLE class (
+	id INTEGER PRIMARY KEY,
+	name TEXT,
+	);
+
+CREATE TABLE object (
+	id INTEGER PRIMARY KEY,
+	name TEXT,
+	)
+
+CREATE TABLE class_object(
+	class_id INTEGER,
+	object_id INTEGER)
+
+-- As an idea, why not to describe inheritance of classes
+--as a new table:
+
+CREATE TABLE class_inheritanse (
+	parrent_class_id INTEGER,
+	child_class_id INTEGER)
+ */
