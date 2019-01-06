c5e8c516f4c7c61a59ea2b27576ae978d9e74d3d
bohdandrahan <bohdan.drahan@gmail.com>
2019-01-06 13:30:01 -0500

    edit ex1, ex2, ex3, ex4

---
+++ ex3.sql
@@ -0,0 +1,7 @@
+INSERT INTO person (id, first_name, last_name, age)
+	VALUES (0, 'John', 'Smith', 20);
+
+INSERT INTO pet (id, name, breed, age, dead)
+	VALUES (0, 'Fluffy','cat',5 ,0);
+
+INSERT INTO pet VALUES (1, 'RUMBA', 'Robot', 1, 1);
