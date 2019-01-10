1463368e08b694c54df9efb080f51b5b8c7feaec
bohdandrahan <bohdan.drahan@gmail.com>
2019-01-09 21:47:29 -0500

    add ex10

--- ex10.sql
+++ ex10.sql
@@ -1,6 +1,6 @@
 SELECT * FROM pet;
 
-UPDATE pet Set name = "John's pet" WHERE id IN (
+UPDATE pet SET name = "John's pet" WHERE id IN (
 	SELECT pet.id
 	FROM pet, person_pet, person
 	WHERE
@@ -8,5 +8,19 @@ UPDATE pet Set name = "John's pet" WHERE id IN (
 	pet.id = person_pet.pet_id AND
 	person.first_name = 'John'
 );
+
+SELECT * FROM pet;
+
+-- Study drills
+
+UPDATE pet SET name = "John's oldest pet" WHERE id IN (
+	SELECT pet.id
+	FROM pet, person_pet, person
+	WHERE
+	person.id = person_pet.person_id AND
+	pet.id = person_pet.pet_id AND
+	person.first_name = 'John'
+	AND pet.age = (SELECT MAX(age) FROM pet)
+	);
 
 SELECT * FROM pet;
